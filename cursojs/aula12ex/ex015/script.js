1db697abf21720cb2b977a09497f4ca8b0100a0f
Junior Oliveira <190500459+Junioroliver7@users.noreply.github.com>
2025-05-12 12:10:28 -0300

    ex verificador de idade

---
+++ cursojs/aula12ex/ex015/script.js
@@ -0,0 +1,50 @@
+function verificar() {
+  var data = new Date()
+  var ano = data.getFullYear()
+  var fano = document.getElementById('txtano')
+  var res = document.getElementById('res')
+  if (fano.value.length == 0 || fano.value > ano) {
+    window.alert('[ERRO] Verifique os dados e tente novamente')
+  } else {
+   var fsex = document.getElementsByName('radsex')
+   var idade = ano - Number(fano.value)
+   var gênero = ''
+   var img = document.createElement('img')
+   img.setAttribute('id', 'foto') 
+   if (fsex[0].checked) {
+       gênero = 'Homem'
+       if (idade >= 0 && idade < 10) {
+        // Criança
+        img.setAttribute('src', 'imagens/criançam.jpg' )
+       } else if (idade < 21) {
+     // Jovem
+        img.setAttribute('src', 'imagens/jovemm.jpg' )
+       } else if (idade < 50) {
+        // Adulto
+        img.setAttribute('src', 'imagens/adultom.jpg')
+       } else {
+        // Idoso
+        img.setAttribute('src', 'imagens/idosom.jpg')
+       }
+   } else if (fsex[1].checked) {
+       gênero = 'Mulher' 
+        if (idade >= 0 && idade < 10) {
+        // Criança
+        img.setAttribute('src', 'imagens/criançaf.jpg' )
+       } else if (idade < 21) {
+     // Jovem
+        img.setAttribute('src', 'imagens/jovemf.jpg' )
+       } else if (idade < 50) {
+        // Adulto
+        img.setAttribute('src', 'imagens/adultof.jpg')
+       } else {
+        // Idoso
+        img.setAttribute('src', 'imagens/idosof.jpg')
+       }
+   }
+   res.style.textAlign = 'center'
+   res.innerHTML = `Detectamos<strong> ${gênero} </strong>com ${idade} anos.`
+   res.appendChild(img)
+ 
+  }
+}
