ce6e42aa8641da03375e51d1343727077b59c82a
Junior Oliveira <190500459+Junioroliver7@users.noreply.github.com>
2025-05-14 11:07:50 -0300

    ex estrutura de repetição

--- cursojs/aula13/ambiente.js
+++ cursojs/aula13/ambiente.js
@@ -1,23 +1,18 @@
 var c = 1
 do {
-    console.log(`Passo${c}`)
-    c++ 
-} while (c <= 6) 
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1
+} while (c <= 10) 
 
 
-
-
-/*
+/* 
 var c = 1
-while (c <= 5) {
-    console.log(`Passo${c}`)
-    c++ 
+while (c <= 3) {
+    console.log(`Passo ${c}`)    // ESTRUTUA DE REPETIÇÃO MAIS USADA NOS PROGRAMAS
+    c++ // c = c + 1
 }
- */
 
-
-
-
+*/
 
 
 
